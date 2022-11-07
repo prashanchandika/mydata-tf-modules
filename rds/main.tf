53fa14a85b64cb3a8f00031f8a8a04dc738b09cf
prashan.lakshan <prashan.lakshan@mydigitaloffice.ca>
2022-11-07 21:53:23 +0530

    rds engine version change ignored

--- rds/main.tf
+++ rds/main.tf
@@ -46,6 +46,14 @@ resource "aws_db_instance" "rds1" {
   tags                = var.tags
 
   apply_immediately   = var.apply_immediately
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      engine_version,
+    ]
+  }
 }
 
 
